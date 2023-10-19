afeb873932fedd7fe9ad55b5dcf9306448ddaeb4
amisu-ctr <hamisurahman@gmail.com>
2023-10-19 12:25:50 +0000

    Nesting Routes

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,11 @@ import { Container } from 'react-bootstrap';
 import { Home } from './pages/Home';
 import { Store } from './pages/Store';
 import { About } from './pages/About';
+import NotFound from './pages/NotFound';
 import Book from './pages/Book';
 import BookList from './pages/BookList';
 import NewBook from './pages/NewBook';
+
 import { Navbar } from './components/Navbar';
 import { ShoppingCartProvider } from './context/ShoppingCartContext';
 
@@ -18,9 +20,20 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/store" element={<Store />} />
           <Route path="/about" element={<About />} />
-          <Route path="/books/" element={<BookList />} />
-          <Route path="/books/:id" element={<Book />} />
-          <Route path="/books/new" element={<NewBook />}></Route>
+          {/**Below was to learn about routing */}
+          <Route path="/books">
+            <Route index element={<BookList />} />
+            <Route path=":id" element={<Book />} />
+            <Route path="new" element={<NewBook />} />
+          </Route>
+          {/* <Route path="/books/" element={<BookList />} />
+          <Route path="/books/:id" element={<Book />} /> {/**dynamic routing */}
+          {/* <Route path="/books/new" element={<NewBook />}></Route>{' '} */}
+          {/**routing
+          priority */}
+          <Route path="*" element={<NotFound />}></Route>
+          {/**Route that matches
+          anything */}
         </Routes>
       </Container>
     </ShoppingCartProvider>
@@ -28,3 +41,9 @@ function App() {
 }
 
 export default App;
+
+/**
+ * A * will match anything at all which makes it perfect for things like a 404 page.
+ * A route that contains a * will also be less specific
+ * than anything else so you will never accidentally match a * route when another route would have also matched.
+ */
---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,4 @@
+function NotFound() {
+  return <div>404 Page does Not exist</div>;
+}
+export default NotFound;
